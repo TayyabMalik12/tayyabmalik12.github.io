d93c2a8ad2b594ac9186e485b9ddc1fdd74e28d0
TayyabMalik12 <50166573+TayyabMalik12@users.noreply.github.com>
2024-10-26 11:07:24 +0500

    virtual environment

--- ninja/app.py
+++ ninja/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, flash
 import smtplib  # Use this for sending emails
 from email.mime.text import MIMEText
+import mimetypes
+mimetypes.add_type('image/webp', '.webp')
 
 app = Flask(__name__)
 app.secret_key = 'tayyab12'
@@ -130,5 +132,5 @@ def send_email(name, email, message):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
