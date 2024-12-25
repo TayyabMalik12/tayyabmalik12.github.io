fa2bd7d6d5ee5ae16a9a736e375c2c42b34e0f4b
TayyabMalik12 <50166573+TayyabMalik12@users.noreply.github.com>
2024-12-25 23:41:22 +0500

    changes front view

--- ninja/app.py
+++ ninja/app.py
@@ -17,7 +17,7 @@ services = [
     {
         "title": "Dry Cleaning",
         "description": "At Manor Lane Dry Cleaners in Hither Green, we take extra care with every item. Before cleaning, we check for stains and spot them with specialized dry cleaning chemicals to clean well. This way, your clothes come out fresh and spotless the first time. Better yet, we offer same-day service—all done on-site in Hither Green.",
-        "images": ["/static/images/dry_cleaning.jpg", "/static/images/dry_cleaning_natives.jpg" ],
+        "images": ["/static/images/dry_cleaning.jpg", "/static/images/dry_clean_1.jpg"],
         "short_description": "Dry Cleaning Done Right! "
     },
     {
@@ -25,6 +25,20 @@ services = [
         "description": "At Manor Lane Dry Cleaners, we offer expert clothing alterations and repairs. Our specialist tailor is available Monday to Saturday, providing on-site fittings for your convenience. From replacing buttons to adjusting jacket lengths—especially wedding dress, jeans zips to patch, jacket, bag zip & repairs and curtain shortening & re-lining. Just bring in your ideas, and we’ll make them happen!",
         "images": ["/static/images/alternations_repairs_onsite.jpg", "/static/images/alternation_repairs.jpg", "/static/images/alternation_repairs_bag.jpg", "/static/images/alternation_repairs_kits.jpg", "/static/images/alternation_repairs_lady.jpg", "/static/images/alternation_repairs_scissor.jpg", "/static/images/alternation_repairs_strips.jpg", "/static/images/alternation_repairs_dress.jpg"],
         "short_description": "Don’t Toss It—Repair or Alter It!"
+    },
+    {
+        "title": "Wedding Dress",
+        "description": "Choosing the right dry cleaner is essential, and our specialized technique ensures your gown is treated with the utmost care. We hand-treat stains, use distilled solvents, clean each dress individually, and press each layer by hand. Your gown will be returned in your wedding dress bag or folded in an acid-free box, ideal for storage or travel.",
+        "images": ["/static/images/wedding_dress.jpg", "/static/images/wedding_dress1.jpg",
+                   "/static/images/wedding_dress2.jpg"],
+        "short_description": "Choose Manor Lane for your wedding dress care! "
+    },
+    {
+        "title": "Natives Cleaning",
+        "description": "We specialize in cleaning all types of traditional and native attire. From Scottish kilt to African native and Asian sari, we provide professional dry-cleaning services for all.",
+        "images": ["/static/images/natives_clean1.jpg", "/static/images/natives_clean2.jpg", "/static/images/natives_clean3.jpg"],
+        "short_description": ""
+
     },
     {
         "title": "Ironing Service",
@@ -53,28 +67,23 @@ services = [
     {
         "title": "Skiwear Cleaning",
         "description": "We clean everything from suits and jackets to trousers, gloves, hats, sleeping bags and tents. We specialize in cleaning delicate materials and luxury brands, down jackets and coats, including Canada Goose and Moncler, leaving them fresh and looking like new and fluffy.",
-        "images": ["/static/images/dress_cleaning.jpg", ],
+        "images": ["/static/images/skiwear1.jpg", ],
         "short_description": "Luxury brands  "
     },
     {
         "title": "Dye Service",
         "description": "With a range of popular colors in stock, we can dye most items that are safe for water treatment—cotton works best for vibrant results.",
-        "images": ["/static/images/dye_service.jpg"],
+        "images": ["/static/images/dye_service.jpg", "/static/images/dress_cleaning.jpg"],
         "short_description": "We can even dye your clothes!"
     },
     {
         "title": "School Uniform",
         "description": "We clean all types of school uniforms—from skirts to blazers—at competitive prices, with special offers available year-round. Plus, kids get a sweet treat on us.",
-        "images": ["/static/images/uniform.jpg"],
+        "images": ["/static/images/uniform.jpg", "/static/images/school_blazer.jpg", "/static/images/school_shirt.jpg" ],
         "short_description": "Keep uniforms looking their best!"
 
     },
-    {
-        "title": "Wedding Dress",
-        "description": "Choosing the right dry cleaner is essential, and our specialized technique ensures your gown is treated with the utmost care. We hand-treat stains, use distilled solvents, clean each dress individually, and press each layer by hand. Your gown will be returned in your wedding dress bag or folded in an acid-free box, ideal for storage or travel.",
-        "images": ["/static/images/wedding_dress.jpg", "/static/images/wedding_dress1.jpg", "/static/images/wedding_dress2.jpg"],
-        "short_description": "Choose Manor Lane for your wedding dress care! "
-    },
+
     {
         "title": "Shoe Repairs",
         "description": "Our skilled cobbler ensures top-quality repairs using only premium materials.",
@@ -113,22 +122,25 @@ price_list = [
     ]
 slides = [
     {
-        'image': '/static/images/back1.png',
+        'image': '/static/images/front1.png',
     },
     {
-        'image': '/static/images/back2.png',
+        'image': '/static/images/front2.png',
     },
     {
-        'image': '/static/images/back3.png',
+        'image': '/static/images/front3.png',
     }
 ]
 
 media_items = [
         {'type': 'video', 'src': '/static/images/wedding_dress_cleaning.mp4', 'thumbnail': '/static/images/thumbnail_wedding_dress_cleaning.png', 'alt': 'Wedding Dress Cleaning', 'title': 'Wedding Dress Cleaning'},
+        {'type': 'video', 'src': '/static/images/wedding_dress_clean.mp4', 'thumbnail': '/static/images/thumbnail_wedding_dress_cleaning.png', 'alt': 'Wedding Dress Cleaning', 'title': 'Wedding Dress Cleaning'},
+        {'type': 'video', 'src': '/static/images/sewing.mp4', 'thumbnail': '/static/images/sewing.png', 'alt': 'Alternations', 'title': 'Alternations & Repairs'},
         {'type': 'video', 'src': '/static/images/toy_before_repair.mp4', 'thumbnail': '/static/images/thumbnail_before_repair.png', 'alt': 'Toy Before Repair', 'title': 'Toy Before Repair'},
         {'type': 'video', 'src': '/static/images/toy_after_repair.mp4', 'thumbnail': '/static/images/thumbnail_after_repair.png', 'alt': 'Toy After Repair', 'title': 'Toy After Repair'},
         {'type': 'video', 'src': '/static/images/native_dress_cleaning.mp4', 'thumbnail': '/static/images/thumbnail_dress_cleaning.png', 'alt': 'Native Dress Cleaning', 'title': 'Native Dress Cleaning'},
-        {'type': 'video', 'src': '/static/images/cushion_cover_making.mp4', 'thumbnail': '/static/images/thumbnail_cushion_making.png', 'alt': 'Cushion Making', 'title': 'Cushion Making'}
+        {'type': 'video', 'src': '/static/images/cushion_cover_making.mp4', 'thumbnail': '/static/images/thumbnail_cushion_making.png', 'alt': 'Cushion Making', 'title': 'Cushion Making'},
+
     ]
 
 @app.route('/')
